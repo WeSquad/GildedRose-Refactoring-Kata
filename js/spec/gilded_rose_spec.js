956e3633a2c4e3a4b8c840fb49730a727a0dd1bd
Sébastien de Beauffort <s.debeauffort@zurich.com>
2018-10-08 11:38:08 +0200

    refactoring init

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,16 +1,36 @@
 describe("Gilded Rose", function() {
 
-  it('should decrease sell in value at the end of the day', function() {
-    const name = 'test';
-    const sellIn = 10;
-    const quality = 10;
-    const item = new Item(name, sellIn, quality);
-    const shop = new Shop();
+  let name = 'test';
+  let sellIn = 10;
+  let quality = 10;
+  let item = new Item(name, sellIn, quality);
+  let shop = new Shop();
+  shop.items.push(item);
+
+  init = function() {
+    name = 'test';
+    sellIn = 10;
+    quality = 10;
+    item = new Item(name, sellIn, quality);
+    shop = new Shop();
     shop.items.push(item);
+    return shop;
+  }
+
+  it('should decrease sell in value at the end of the day', function() {
+    shop = init();
 
     shop.updateQuality();
 
     expect(shop.items[0].sellIn).toEqual(sellIn-1);
+  });
+
+  it('should decrease quality value at the end of the day', function() {
+    shop = init();
+
+    shop.updateQuality();
+
+    expect(shop.items[0].quality).toEqual(quality-1);
   });
 
 });
